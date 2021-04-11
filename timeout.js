711145bc5ec4c839fce40edf0f0b1c06b3b34ba5
ApoloGH <apolo-gh@users.noreply.github.com>
2021-04-10 22:16:25 -0400

    refactor: format and lint

--- timeout.js
+++ timeout.js
@@ -1,53 +1,47 @@
-const log = require('logger')(module)
-let window = require('browser').window
-let Promise = window.Promise || require('promise-polyfill')
-let utils = require('utils')
-
-let ERRORS = {
-  TIMEOUT: 'TimeoutPromiseError',
-  INITIALIZATION: 'TimeoutPromiseInitializationError'
-}
+const ERRORS = {
+  TIMEOUT: 'TimeoutError',
+  INITIALIZATION: 'TimeoutInitializationError',
+};
 
 /**
  * More convenient way to work with timeouts
  * @class
  * @param {number} timeout - number of milliseconds
  * @example
- * let timeout = new TimeoutPromise(1000); // 1 sec
+ * let timeout = new Timeout(1000); // 1 sec
  * timeout.start().catch(function(reason) {
  *     // timeout exceeded
  * });
  */
-let TimeoutPromise = function (timeout) {
-  let timeoutId
-  let resolveTimeout
-  let rejectTimeout
-  let promise = new Promise(function (resolve, reject) {
-    resolveTimeout = resolve
-    rejectTimeout = reject
-  })
+const Timeout = function (timeout) {
+  let timeoutId;
+  let resolveTimeout;
+  let rejectTimeout;
+  const promise = new Promise((resolve, reject) => {
+    resolveTimeout = resolve;
+    rejectTimeout = reject;
+  });
 
   /**
    * Starts timer.
    * @returns {Promise} - timeout promise
    * @example
-   * let timeout = new TimeoutPromise(2000); // 2 sec
+   * let timeout = new Timeout(2000); // 2 sec
    * timeout.start().catch(function(reason) {
    *     // timeout exceeded
    * });
    */
   this.start = function () {
     if (timeout && !timeoutId) {
-      log.debug('start', timeout)
-      timeoutId = window.setTimeout(function () {
-        let error = new Error('Operation timeout exceeded')
+      timeoutId = setTimeout(() => {
+        const error = new Error('Operation timeout exceeded');
 
-        error.name = ERRORS.TIMEOUT
-        rejectTimeout(error)
-      }, timeout)
+        error.name = ERRORS.TIMEOUT;
+        rejectTimeout(error);
+      }, timeout);
     }
-    return promise
-  }
+    return promise;
+  };
 
   /**
    * Stops timer and resolves timeout promise.
@@ -55,19 +49,18 @@ let TimeoutPromise = function (timeout) {
    */
   this.stop = function () {
     if (timeoutId) {
-      log.debug('stop', timeout)
-      window.clearTimeout(timeoutId)
-      timeout = undefined
-      timeoutId = undefined
-      resolveTimeout()
+      clearTimeout(timeoutId);
+      timeout = undefined;
+      timeoutId = undefined;
+      resolveTimeout();
     }
-    return promise
-  }
+    return promise;
+  };
 
   /**
    * Additional way to catch error.
    * @example
-   * let timeout = new TimeoutPromise(2000); // 2 sec
+   * let timeout = new Timeout(2000); // 2 sec
    * ...
    * timeout.start();
    * ...
@@ -75,20 +68,20 @@ let TimeoutPromise = function (timeout) {
    *     // timeout exceeded
    * });
    */
-  this.catch = promise.catch.bind(promise)
+  this.catch = promise.catch.bind(promise);
 
   if (timeout === undefined) {
-    resolveTimeout()
-  } else if (!utils.isInteger(timeout) || timeout <= 0) {
-    let error = new Error('Timeout value must be positive integer')
+    resolveTimeout();
+  } else if (!Number.isInteger(timeout) || timeout <= 0) {
+    const error = new Error('Timeout value must be positive integer');
 
-    error.name = ERRORS.INITIALIZATION
-    rejectTimeout(error)
+    error.name = ERRORS.INITIALIZATION;
+    rejectTimeout(error);
   } else {
-    log.debug('created', timeout)
+    // log.debug('created', timeout);
   }
-}
+};
 
-TimeoutPromise.ERRORS = ERRORS
+Timeout.ERRORS = ERRORS;
 
-module.exports = TimeoutPromise
+module.exports = Timeout;
